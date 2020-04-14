0c789d907456e80b9c211b04adfd4bd4e79b1ff2
Vaidas Datenis <datenis.v@gmail.com>
2020-04-14 13:52:45 +0300

    env
    taking a risk

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD0YIFtQxcd8eLvq0mQrQ1t0--ndDqAERI',
+    authDomain: 'corona-statistics-54724.firebaseapp.com',
+    databaseURL: 'https://corona-statistics-54724.firebaseio.com',
+    projectId: 'corona-statistics-54724',
+    storageBucket: 'corona-statistics-54724.appspot.com',
+    messagingSenderId: '816368828754',
+    appId: '1:816368828754:web:6553aaac7fdb876d513a8b',
+    measurementId: 'G-Y6V4CS8S0Y'
+  }
+};
+
