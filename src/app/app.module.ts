3db76c12af040854a68c7b0472d84ae0f0d166e6
Vaidas Datenis <datenis.v@gmail.com>
2020-04-24 18:35:42 +0300

    Small file changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { environment } from 'src/environments/enviroment';
+import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
--- src/app/shared/services/mapbox.service.ts
+++ src/app/shared/services/mapbox.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/enviroment';
+import { environment } from '../../../environments/environment';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { GeoJson } from '../../layout/map/jobs';
 import * as mapboxgl from 'mapbox-gl';
